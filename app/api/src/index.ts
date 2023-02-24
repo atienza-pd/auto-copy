f21875d9ba08099e540ab912dc534704fb660e02
Darren Atienza <datienza@doehle-mnl.com>
2023-02-24 22:48:16 +0800

    update api and ui

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,19 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Todo } from "types";
+import cors from "cors";
 import listCopyEntryRoutes from "./copy-path/listCopyPathRoutes";
 import addCopyPathRoutes from "./copy-path/addCopyPathRoutes";
 import editCopyPathRoutes from "./copy-path/editCopyPathRoutes";
 import removeCopyPathRoutes from "./copy-path/removeCopyPathRoutes";
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+  exposedHeaders: ["Content-Length"],
+  allowedHeaders: ["Accept", "Content-Type", "Origin", "X-Requested-With"],
+};
+
 const app = express();
 const port = 3000;
-
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use("/copy-path", [
   listCopyEntryRoutes,
   addCopyPathRoutes,
   editCopyPathRoutes,
-  removeCopyPathRoutes
+  removeCopyPathRoutes,
 ]);
 
 app.listen(port, () => {
