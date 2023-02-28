8756a5d41dbb57156ada878ee9ab4a62a02d4fb8
Darren Atienza <datienza@doehle-mnl.com>
2023-02-28 15:23:53 +0800

    ui | add edit page

--- app/ui/src/app/app-routing.module.ts
+++ app/ui/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditCopyPathContainerComponent } from './edit-copy-path/edit-copy-path-container/edit-copy-path-container.component';
 import { AddCopyPathContainerComponent } from './add-copy-path/add-copy-path-container/add-copy-path-container.component';
 import { ListCopyPathContainerComponent } from './list-copy-path/list-copy-path-container/list-copy-path-container.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: ListCopyPathContainerComponent },
   { path: 'add', component: AddCopyPathContainerComponent },
+  { path: 'edit/:id', component: EditCopyPathContainerComponent },
 ];
 
 @NgModule({
---
+++ app/ui/src/app/edit-copy-path/edit-copy-path-container/edit-copy-path-container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditCopyPathContainerComponent } from './edit-copy-path-container.component';
+
+describe('EditCopyPathContainerComponent', () => {
+  let component: EditCopyPathContainerComponent;
+  let fixture: ComponentFixture<EditCopyPathContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditCopyPathContainerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EditCopyPathContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
