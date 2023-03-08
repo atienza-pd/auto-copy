8c73c003bd01679b961cc7c2631532a842e191e5
Darren Atienza <datienza@doehle-mnl.com>
2023-03-08 11:29:18 +0800

    #23 execute copy by day of week

--- copy.ps1
+++ copy.ps1
@@ -1,10 +1,13 @@
-$pathsJson = Get-Paths-Json
+
+$pathsJson = Get-Runnable-Paths-Json
 
 $logPath = Get-LogFile-Path
 
 ForEach ($path in $pathsJson) {
    $sourcePath = Get-Single-Slash-Path -path $path.source;
    $destinationPath =  Get-Single-Slash-Path -path $path.destination;
+   $name = $path.name;
+   Write-Host "Name $name"
    Write-Host "Source: $sourcePath"
    Write-Host "Destination: $destinationPath"
    robocopy $sourcePath $destinationPath $path.includeFilesOnly /log+:$logPath /np /xo /mt /z /s /purge /xd $path.excludeDirectories /xf $path.excludeFiles
