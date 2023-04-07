85ae4d49e969108b6fecd7565663b0e46cd0e7b9
Darren Atienza <datienza@doehle-mnl.com>
2023-04-07 11:04:15 +0800

    #49 UI | Add edit active days of week

--- app/ui/src/app/edit-copy-path/components/edit-copy-path-form/edit-copy-path-form.component.ts
+++ app/ui/src/app/edit-copy-path/components/edit-copy-path-form/edit-copy-path-form.component.ts
@@ -1,78 +1,91 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { CopyPathDto } from "../../../../../../api/src/copy-path/copyPathDto";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CopyPathDto } from '../../../../../../api/src/copy-path/copyPathDto';
 
 @Component({
-    selector: "app-edit-copy-path-form",
-    templateUrl: "./edit-copy-path-form.component.html",
-    styleUrls: ["./edit-copy-path-form.component.scss"],
+  selector: 'app-edit-copy-path-form',
+  templateUrl: './edit-copy-path-form.component.html',
+  styleUrls: ['./edit-copy-path-form.component.scss'],
 })
 export class EditCopyPathFormComponent implements OnInit {
-    @Input() copyPath!: CopyPathDto;
+  @Input() copyPath!: CopyPathDto;
+    showAddActiveDaysOfWeekModal = false;
 
-    onRemoveExcludedDirectories(index: number) {
-        this.copyPath.excludeDirectories.splice(index, 1);
-    }
-    onHideAddExcludedDiretoryModal() {
-        this.showAddExcludedDirectoryModal = false;
-    }
-    onOkAddExcludedDiretoryModal(name: string) {
-        this.copyPath.excludeDirectories.push(name);
-        this.showAddExcludedDirectoryModal = false;
-    }
-    showAddExcludedFileModal!: boolean;
-    showAddExcludedDirectoryModal!: boolean;
+  onRemoveExcludedDirectories(index: number) {
+    this.copyPath.excludeDirectories.splice(index, 1);
+  }
+  onHideAddExcludedDiretoryModal() {
+    this.showAddExcludedDirectoryModal = false;
+  }
+  onOkAddExcludedDiretoryModal(name: string) {
+    this.copyPath.excludeDirectories.push(name);
+    this.showAddExcludedDirectoryModal = false;
+  }
+  showAddExcludedFileModal!: boolean;
+  showAddExcludedDirectoryModal!: boolean;
 
-    onHideAddExcludedFileModal() {
-        this.showAddExcludedFileModal = false;
-    }
-    onOkAddExcludedFileModal(name: string) {
-        this.copyPath.excludeFiles.push(name);
-        this.showAddExcludedFileModal = false;
-    }
+  onHideAddExcludedFileModal() {
+    this.showAddExcludedFileModal = false;
+  }
+  onOkAddExcludedFileModal(name: string) {
+    this.copyPath.excludeFiles.push(name);
+    this.showAddExcludedFileModal = false;
+  }
 
-    onRemoveIncludedFiles(index: number) {
-        this.copyPath.includeFilesOnly.splice(index, 1);
-    }
-    onRemoveExcludedFiles(index: number) {
-        this.copyPath.excludeFiles.splice(index, 1);
-    }
-    onOkAddIncludeFileModal(name: string) {
-        this.copyPath.includeFilesOnly.push(name);
-        this.showAddIncludeFileModal = false;
-    }
-    onHideAddIncludeFileModal() {
-        this.showAddIncludeFileModal = false;
-    }
-    showAddIncludeFileModal = false;
-    addExcludeFiles() {
-        this.showAddExcludedFileModal = true;
-    }
-    addExcludeDirectories() {
-        this.showAddExcludedDirectoryModal = true;
-    }
-    addIncludeFile() {
-        this.showAddIncludeFileModal = true;
-    }
+  onRemoveIncludedFiles(index: number) {
+    this.copyPath.includeFilesOnly.splice(index, 1);
+  }
+  onRemoveExcludedFiles(index: number) {
+    this.copyPath.excludeFiles.splice(index, 1);
+  }
+  onOkAddIncludeFileModal(name: string) {
+    this.copyPath.includeFilesOnly.push(name);
+    this.showAddIncludeFileModal = false;
+  }
+  onHideAddIncludeFileModal() {
+    this.showAddIncludeFileModal = false;
+  }
+  showAddIncludeFileModal = false;
+  addExcludeFiles() {
+    this.showAddExcludedFileModal = true;
+  }
+  addExcludeDirectories() {
+    this.showAddExcludedDirectoryModal = true;
+  }
+  addIncludeFile() {
+    this.showAddIncludeFileModal = true;
+  }
 
-    @Output() submitForm = new EventEmitter();
+  @Output() submitForm = new EventEmitter();
 
-    onSubmitForm() {
-        this.copyPath.name = this.validateForm.controls["name"].value;
-        this.copyPath.source = this.validateForm.controls["source"].value;
-        this.copyPath.destination =
-            this.validateForm.controls["destination"].value;
-        this.submitForm.emit(this.copyPath);
-    }
+  onSubmitForm() {
+    this.copyPath.name = this.validateForm.controls['name'].value;
+    this.copyPath.source = this.validateForm.controls['source'].value;
+    this.copyPath.destination = this.validateForm.controls['destination'].value;
+    this.submitForm.emit(this.copyPath);
+  }
+  addActiveDaysOfWeek() {
+    this.showAddActiveDaysOfWeekModal = true;
+  }
+  onRemoveActiveDaysOfWeek(index: number) {
+    this.copyPath.activeDaysOfWeek.splice(index, 1);
+  }
 
-    validateForm!: FormGroup;
-    constructor(private fb: FormBuilder) {}
+  onOkAddActiveDaysOfWeekModal(day: string) {
+    this.copyPath.activeDaysOfWeek.push(day);
+    this.showAddExcludedDirectoryModal = false;
+  }
+  onHideAddActiveDaysOfWeekModal() {
+    this.showAddActiveDaysOfWeekModal = false;
+  }
+  validateForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-    ngOnInit(): void {
-        this.validateForm = this.fb.group({
-            name: [this.copyPath.name, [Validators.required]],
-            source: [this.copyPath.source, [Validators.required]],
-            destination: [this.copyPath.destination, [Validators.required]],
-        });
-    }
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      name: [this.copyPath.name, [Validators.required]],
+      source: [this.copyPath.source, [Validators.required]],
+      destination: [this.copyPath.destination, [Validators.required]],
+    });
+  }
 }
