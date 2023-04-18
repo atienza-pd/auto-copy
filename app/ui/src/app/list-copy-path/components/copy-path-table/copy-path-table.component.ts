4877ac2400d732786758977408a24c6329de3005
Darren Atienza <datienza@doehle-mnl.com>
2023-04-18 21:43:22 +0800

    #74 ui | add filter and sort on table

--- app/ui/src/app/list-copy-path/components/copy-path-table/copy-path-table.component.ts
+++ app/ui/src/app/list-copy-path/components/copy-path-table/copy-path-table.component.ts
@@ -1,18 +1,46 @@
 import { CopyPathDto } from "../../../../../../api/src/copy-path/copyPathDto";
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { NzButtonSize } from "ng-zorro-antd/button";
-interface Person {
-    key: string;
+import { NzTableFilterFn, NzTableFilterList, NzTableSortFn, NzTableSortOrder } from 'ng-zorro-antd/table';
+interface DataItem {
     name: string;
-    age: number;
-    address: string;
+    source: string;
+    destination: string;
+    includeFilesOnly: string;
+    excludeDirectories: string;
+    excludeFiles: string;
 }
+
+interface ColumnItem {
+    name: string;
+    sortOrder: NzTableSortOrder | null;
+    sortFn: NzTableSortFn<DataItem> | null;
+    listOfFilter: NzTableFilterList;
+    filterFn: NzTableFilterFn<DataItem> | null;
+  }
+
 @Component({
     selector: "app-copy-path-table",
     templateUrl: "./copy-path-table.component.html",
     styleUrls: ["./copy-path-table.component.scss"],
 })
 export class CopyPathTableComponent {
+    trackByName(_: number, item: ColumnItem): string {
+        return item.name;
+      }
+    listOfColumns: ColumnItem[] = [
+        {
+          name: 'Name',
+          sortOrder: null,
+          sortFn: (a: DataItem, b: DataItem) => a.name.localeCompare(b.name),
+          listOfFilter: [
+            { text: 'Online', value: 'Online' },
+            { text: 'Local', value: 'Local' },
+            { text: 'Network', value: 'Network' }
+          ],
+          filterFn: (list: string[], item: DataItem) => list.some(name => item.name.indexOf(name) !== -1)
+        }
+      ];
     @Input() copyPaths: CopyPathDto[] = [];
     @Output() showRemoveCopyPathModal = new EventEmitter();
     size: NzButtonSize = "large";
