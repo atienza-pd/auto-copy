3d0d1f7b9a40b118ca0342ea02f7f155fe3ddc6f
Darren Atienza <datienza@doehle-mnl.com>
2024-11-24 16:01:02 +0800

    #101 upgrade to latest angular version

--- app/ui/src/app/add-copy-path/add-copy-path-container/add-copy-path-container.component.ts
+++ app/ui/src/app/add-copy-path/add-copy-path-container/add-copy-path-container.component.ts
@@ -1,21 +1,22 @@
-import { CopyPathDto } from "../../../../../api/src/copy-path/copyPathDto";
-import { Component } from "@angular/core";
-import { AddCopyPathHttpService } from "../add-copy-path-http/add-copy-path-http.service";
-import { Router } from "@angular/router";
-@Component({ standalone: false,
-    selector: "app-add-copy-path-container",
-    templateUrl: "./add-copy-path-container.component.html",
-    styleUrls: ["./add-copy-path-container.component.scss"],
+import { CopyPathDto } from '../../../../../api/src/copy-path/copyPathDto';
+import { Component } from '@angular/core';
+import { AddCopyPathHttpService } from '../add-copy-path-http/add-copy-path-http.service';
+import { Router } from '@angular/router';
+@Component({
+  standalone: false,
+  selector: 'app-add-copy-path-container',
+  templateUrl: './add-copy-path-container.component.html',
+  styleUrls: ['./add-copy-path-container.component.scss'],
 })
 export class AddCopyPathContainerComponent {
-    onSubmitForm(data: CopyPathDto) {
-        this.httpService.execute(data).subscribe(() => {
-            this.router.navigate([".."]);
-        });
-    }
+  onSubmitForm(data: CopyPathDto) {
+    this.httpService.execute(data).subscribe(() => {
+      this.router.navigate(['..']);
+    });
+  }
 
-    constructor(
-        private httpService: AddCopyPathHttpService,
-        private router: Router
-    ) {}
+  constructor(
+    private httpService: AddCopyPathHttpService,
+    private router: Router
+  ) {}
 }
