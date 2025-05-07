78dc65de42fadff09a825837c02659e562eb7d29
Darren Atienza <datienza@doehle-mnl.com>
2025-05-07 17:33:44 +0800

    #110 ui | Use Modal Service for active days modal

--- app/ui/src/app/features/copy-paths/add-copy-path/components/add-copy-path-form/add-copy-path-form.component.ts
+++ app/ui/src/app/features/copy-paths/add-copy-path/components/add-copy-path-form/add-copy-path-form.component.ts
@@ -9,12 +9,12 @@ import { CopyPathDto } from '../../../models';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { AddCopyPathAddActiveDaysOfWeekModalComponent } from '../add-copy-path-add-active-days-of-week-modal/add-copy-path-add-active-days-of-week-modal.component';
 import { CommonModule } from '@angular/common';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { FileFormModalComponent } from '../../../shared/components/file-form-modal/file-form-modal.component';
 import { DirectoryFormModalComponent } from '../../../shared/components/directory-form-modal/directory-form-modal.component';
+import { ActiveDaysOfWeekModalComponent } from '../../../shared/components/active-days-of-week-modal/active-days-of-week-modal.component';
 
 @Component({
   standalone: true,
@@ -24,7 +24,6 @@ import { DirectoryFormModalComponent } from '../../../shared/components/director
     NzFormModule,
     NzListModule,
     NzButtonModule,
-    AddCopyPathAddActiveDaysOfWeekModalComponent,
     NzInputModule,
   ],
   selector: 'app-add-copy-path-form',
@@ -37,13 +36,11 @@ export class AddCopyPathFormComponent implements OnInit {
   onRemoveActiveDaysOfWeek(index: number) {
     this.copyPath.activeDaysOfWeek.splice(index, 1);
   }
-  onHideAddActiveDaysOfWeekModal() {
-    this.showAddActiveDaysOfWeekModal = false;
-  }
+
   onOkAddActiveDaysOfWeekModal(day: string) {
     this.copyPath.activeDaysOfWeek.push(day);
   }
-  showAddActiveDaysOfWeekModal: boolean = false;
+
   onRemoveExcludedDirectories(index: number) {
     this.copyPath.excludeDirectories.splice(index, 1);
   }
@@ -62,6 +59,40 @@ export class AddCopyPathFormComponent implements OnInit {
 
   onRemoveExcludedFiles(index: number) {
     this.copyPath.excludeFiles.splice(index, 1);
+  }
+
+  public addActiveDaysOfWeek(): void {
+    const modal = this.modalService.create({
+      nzTitle: 'Add Active Days Of Week',
+      nzContent: ActiveDaysOfWeekModalComponent,
+      nzData: {
+        selectedDays: this.copyPath.activeDaysOfWeek,
+      },
+      nzFooter: [
+        {
+          label: 'Close',
+          onClick: () => modal.destroy(),
+        },
+        {
+          label: 'Confirm',
+          type: 'primary',
+          onClick: (component) => {
+            const activeDays =
+              component
+                ?.daysOfWeek()
+                .filter((x) => x.checked)
+                .map((y) => y.value) ?? [];
+
+            this.copyPath.activeDaysOfWeek = [...activeDays];
+            modal.destroy();
+          },
+        },
+      ],
+      nzCentered: true,
+      nzWidth: '600px',
+      nzStyle: { top: '20px' },
+      nzBodyStyle: { padding: '2em' },
+    });
   }
 
   public addExcludedDirectory(): void {
@@ -169,10 +200,6 @@ export class AddCopyPathFormComponent implements OnInit {
     this.copyPath.source = this.validateForm.controls['source'].value;
     this.copyPath.destination = this.validateForm.controls['destination'].value;
     this.submitForm.emit(this.copyPath);
-  }
-
-  addActiveDaysOfWeek() {
-    this.showAddActiveDaysOfWeekModal = true;
   }
 
   validateForm!: FormGroup;
